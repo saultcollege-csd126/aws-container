fcac8a13e24249961339db08bafe139fed4f4f6b
Rodney Martin <rodney.martin@saultcollege.ca>
2026-01-12 19:31:22 +0000

    Initial commit

---
+++ .devcontainer/on_create.sh
@@ -0,0 +1,20 @@
+# This script gets run as devcontainer post-create command
+
+set -eux  # Exit on error and print commands
+
+# Make sure AWS credentials file exists with correct permissions
+mkdir -p .aws
+touch .aws/credentials
+chmod 600 .aws/credentials
+
+# Install Python packages
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install --upgrade pip
+
+echo '
+# Auto-activate project venv if present
+if [ -f /workspaces/aws-deployment-project/.venv/bin/activate ]; then
+    source /workspaces/aws-deployment-project/.venv/bin/activate
+fi
+' >> ~/.bashrc
---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Container
+
+This repository provides a Docker container pre-configured with the AWS CLI and other useful tools for managing AWS services.
