486316da1592b33f6ec680b6d02d37985dd7a3a0
Claire Page <22026811@saultcollege.ca>
2026-03-25 03:00:07 +0000

    doneee

--- app/flask/src/aws.py
+++ app/flask/src/aws.py
@@ -8,4 +8,4 @@ _session = boto3.session.Session(region_name=REGION)
 
 def client(service_name):
     """Get a boto3 client for the specified AWS service."""
-    return _session.client(service_name)
+    return _session.client(service_name)    
